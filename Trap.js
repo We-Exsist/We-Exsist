a8e83c6a93ae1b1bac72836f97240fb0873df4fa
We-Exist <redstonemack@gmail.com>
2018-03-07 13:27:56 -0500

    Spaces!

--- Trap.js
+++ Trap.js
@@ -1,7 +1,7 @@
 var x = 1;
 while (0 !== x) {
     var y = 20 - x;
-    alert("Get ready for" + y + "more!");
+    alert("Get ready for " + y + " more!");
     x = x + 1;
     if (x === 20) { break; }
 }
