c19d57f1d311984e035d79c4a2187522caeb834c
We-Exist <redstonemack@gmail.com>
2018-03-07 12:47:35 -0500

    Save

--- Trap.js
+++ Trap.js
@@ -1,3 +1,7 @@
-while (true !== false) {
-    window.alert("Get ready for many more!");
+var x = 1;
+while (0 !== x) {
+    var y = 20 - x;
+    alert("Get ready for" + y + "more!");
+    x = x + 1;
+    if (x === 20) { break; }
 }
