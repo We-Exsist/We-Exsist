2eb23ef183e567e3de6b8df55776a3bd32e95495
We-Exist <redstonemack@gmail.com>
2018-03-10 13:22:42 -0500

    I believe this is probably the last malware demonstration commit.

--- Trap.js
+++ Trap.js
@@ -3,5 +3,6 @@ while (0 !== x) {
     var y = 20 - x;
     alert("Get ready for " + y + " more!");
     x = x + 1;
-    if (x === 20) { break; }
+    if (x === 20) { if (confirm("If this were real this would most likely go on forever. Would you like to restart the while loop?");) { break; } else { x = 0; } }
 }
+alert("The word document's macro virus demonstration doesn't work quite right. I am not proficient in Visual Basic, so I cannot reliably debug this problem.");
